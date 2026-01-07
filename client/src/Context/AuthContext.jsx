3fc29ac1b75f55f1136fed05a5c77aa42473e50c
Abhishek <149757033+abhiJunior@users.noreply.github.com>
2026-01-07 18:52:37 +0530

    Change BASE_URL to production server
    Updated BASE_URL to point to the production server.

--- client/src/Context/AuthContext.jsx
+++ client/src/Context/AuthContext.jsx
@@ -2,7 +2,7 @@
 import React, { createContext, useContext, useState, useEffect, useCallback } from 'react';
 
 const AuthContext = createContext(null);
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = "https://travel-allowance-for-railways.onrender.com"
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
